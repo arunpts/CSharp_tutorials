b0287d26b8e685ecccf993eebdba0cb435623960
Arun Prakash <57718711+arunpts@users.noreply.github.com>
2023-05-22 14:20:48 +0530

    Add files via upload

---
+++ userinput/userinput/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userinput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Name: {0}\nAge: {1}", name, age);
+        }
+    }
+}
